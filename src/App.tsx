38cbc13d04c9fda4592e8d229b865660df830e7d
Ulises Alanis <ulises.alanis.255@gmail.com>
2025-05-13 18:55:00 -0600

    basic!

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,32 @@
-import PalleteGenerator from './PalletGenerator'
+import PalleteGenerator from "./PalletGenerator";
+import Notes from "./Notes";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Link } from "react-router";
 
+function Options() {
+	return (
+		<div id='utils' className='w-fit bg-sumiInk1 rounded-xl'>
+			<div id='Pallet Generator' className='util'>
+				<Link to="/pallet-generator">
+					<button>Pallet Generator</button>
+				</Link>
+			</div>
+			<div id='Notes' className='util'>
+				<Link to="/notes">
+					<button>Notes</button>
+				</Link>
+			</div>
+		</div>
+	);
+}
 export default function App() {
-  return PalleteGenerator()
+	return (
+		<Router>
+			<Routes>
+				<Route path="/" element={<Options />} />
+				<Route path="/pallet-generator" element={<PalleteGenerator />} />
+				<Route path="/notes" element={<Notes />} />
+			</Routes>
+		</Router>
+	);
 }
--- src/PalletGenerator.tsx
+++ src/PalletGenerator.tsx
@@ -1,45 +1,82 @@
-import {} from './color.ts'
-import { JSX, useState } from 'react'
+import { ChangeEventHandler, useState } from 'react'
+import { oklab_to_rgb, oklch_to_oklab, rgb_to_hex } from './color'
 
-function Slider(color: String): [number, JSX.Element] {
-  const [val, setval] = useState<number>(0)
-  return [
-    val,
-    <div className={`val-color-picker ${color}`}>
-      <div className='light-sel'>
-        {' '}
-        <input
-          type='range'
-          max='1000'
-          min='0'
-          step='1'
-          onChange={(e) => {
-            setval(parseInt(e.target.value) / 1000)
-          }}
-        />
-        <div />
-        <div className='chroma-sel' />
-        <div className='hue-sel' />
-      </div>
-    </div>,
-  ]
+
+function ColorPicker() {
+	const [L, setl] = useState(0.8)
+	const [c, setc] = useState(0.3)
+	const [h, seth] = useState(0)
+
+	const fmt = (
+		beg_l: number,
+		beg_c: number,
+		beg_h: number,
+		end_l: number,
+		end_c: number,
+		end_h: number
+	) => {
+		return `linear-gradient(to right, oklch(${beg_l} ${beg_c} ${beg_h * 360}deg), oklch(${end_l} ${end_c} ${end_h * 360}deg) )`
+	}
+
+	const gradient_maker = () => {
+		let data = ''
+		for (let i = 0; i < 16; ++i) {
+			let deg = Math.floor((i / 16) * 360)
+			data += `oklch(${L} ${c} ${deg}deg),`
+		}
+		data += `oklch(${L} ${c} 360deg)`
+
+		return data
+	}
+
+	const sliderMaker = (def: number, bg_image: string, on: ChangeEventHandler<HTMLInputElement>) => {
+		return <input
+			className='color-picker-slider'
+			type='range'
+			max='1000'
+			min='0'
+			step='1'
+			defaultValue={def * 1000}
+			style={{
+				backgroundImage: bg_image,
+			}}
+			onChange={on}
+		/>
+	};
+
+	const bg_rainbow = `linear-gradient(to right, ${gradient_maker()})`
+	const OkL = sliderMaker(L, fmt(0, c, h, 1, c, h), e => setl(parseInt(e.target.value) / 1000));
+	const Okc = sliderMaker(c, fmt(L, 0, h, L, 1, h), e => setc(parseInt(e.target.value) / 1000));
+	const Okh = sliderMaker(h, bg_rainbow, e => seth(parseInt(e.target.value) / 1000));
+
+	console.log(L, c, h)
+
+	return (
+		<div className='color-picker'>
+			<button className='color-picker-square' style={{ backgroundImage: fmt(L, c, h, L, c, h) }}
+				onClick={_ => {
+					const rgb = oklab_to_rgb(oklch_to_oklab({ L, c, h }));
+					navigator.clipboard.writeText(rgb_to_hex(rgb));
+				}}
+			/>
+			<div className='color-picker-sliders'>
+				{OkL}
+				{Okc}
+				{Okh}
+			</div>
+		</div>
+	)
 }
+export default function PalletGenerator() {
+	return <div id='pallet-generator' >
 
-function ColorGenerator() {
-  const [l, Lslider] = Slider('bg-linear-to-r from-red-100 from-blue-500')
-  const [c, Cslider] = Slider('')
-  const [h, Hslider] = Slider('')
-
-  return (
-    <div>
-      {l}, {c}, {h}
-      {Lslider}
-      {Cslider}
-      {Hslider}
-    </div>
-  )
-}
-
-export default function PalleteGenerator() {
-  return ColorGenerator()
+		<div id='options' >
+		</div>
+		<div id='sliders'>
+			<ColorPicker />
+			<ColorPicker />
+		</div>
+		<div id='colors'>
+		</div>
+	</div>
 }
