0b9388cf78f8ad5b0a0e09af37a3afea71f75417
arch machine <lilith.alanis.255@gmail.com>
2025-05-13 18:55:00 -0600

    basic!

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+createRoot(document.getElementById('root')!).render(
+	<StrictMode>
+		<div className='flex items-center justify-center h-screen w-fir max-w-4/5 m-auto md:ml-4 md:justify-start'>
+			<App />
+		</div>
+	</StrictMode>
+)
