c1691284b93a07065ae10fce330182a2dccabab0
Ulises Alanis <ulises.alanis.255@gmail.com>
2025-05-27 00:30:51 -0600

    dumb way to create notes

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,22 +1,22 @@
+import { HomeIcon } from '@heroicons/react/16/solid';
 import { Link } from 'react-router';
 
 type HeaderProp = {
-	name: string;
-	ret?: any;
+  name: string;
+  ret?: any;
 };
 
 export default function Header({ name, ret }: HeaderProp) {
-	return (
-		<header className='text-fujiWhite bg-sumiInk4 flex h-fit justify-between rounded-b-2xl p-3 pr-5 pl-5 text-3xl'>
-			<h1> {name} </h1>
-			{ret !== undefined ? (
-				<Link className='' to='/'>
-					{' '}
-					Home{' '}
-				</Link>
-			) : (
-				<div />
-			)}
-		</header>
-	);
+  return (
+    <header className='text-fujiWhite bg-sumiInk4 flex h-fit justify-between rounded-b-2xl p-3 pr-5 pl-5 text-3xl'>
+      <h1> {name} </h1>
+      {ret !== undefined ? (
+        <Link className='align-middle' to='/'>
+          <HomeIcon className='text-fujiWhite h-8' />
+        </Link>
+      ) : (
+        <div />
+      )}
+    </header>
+  );
 }
