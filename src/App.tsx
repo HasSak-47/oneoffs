0c2651f4fcce55fba0fdd1bfbab9568d08a1c0d4
Ulises Alanis <ulises.alanis.255@gmail.com>
2025-05-26 23:27:43 -0600

    forgot to set the correct url :)

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,14 @@ import PalletGenerator from './PalletGenerator';
 import Options from './Options';
 
 export default function App() {
-	return (
-		<Router>
-			<Routes>
-				<Route path='/' element={<Options />} />
-				<Route path='/tetris' element={<Tetris />} />
-				<Route path='/PalletGenerator' element={<PalletGenerator />} />
-				<Route path='/notes' element={<Notes />} />
-			</Routes>
-		</Router>
-	);
+  return (
+    <Router>
+      <Routes>
+        <Route path='/oneoffs/' element={<Options />} />
+        <Route path='/oneoffs/tetris' element={<Tetris />} />
+        <Route path='/oneoffs/PalletGenerator' element={<PalletGenerator />} />
+        <Route path='/oneoffs/notes' element={<Notes />} />
+      </Routes>
+    </Router>
+  );
 }
