24a419fed9709bf493781314302e7dcb9f05814e
Ulises Alanis <ulises.alanis.255@gmail.com>
2025-05-27 15:57:48 -0600

    finished blog (i think) and made it more fancy

--- src/Header.tsx
+++ src/Header.tsx
@@ -4,22 +4,31 @@ import { Link } from 'react-router';
 type HeaderProp = {
   name: string;
   ret?: any;
+  extra?: any;
 };
 
 /*
  * basic dumb header that will be used everywhere!
  */
-export default function Header({ name, ret }: HeaderProp) {
+export default function Header({ name, ret, extra }: HeaderProp) {
   return (
-    <header className='text-fujiWhite bg-sumiInk4 flex h-fit justify-between rounded-b-2xl p-3 pr-5 pl-5 text-3xl'>
-      <h1> {name} </h1>
-      {ret !== undefined ? (
-        <Link className='align-middle' to='/'>
-          <HomeIcon className='text-fujiWhite h-8' />
-        </Link>
-      ) : (
-        <div />
-      )}
+    <header className='bg-sumiInk4 text-fujiWhite flex items-center justify-between rounded-b-2xl px-6 py-4 shadow-md'>
+      <h1 className='text-3xl font-bold tracking-tight'>{name}</h1>
+
+      <div className='flex items-center gap-3'>
+        {extra}
+
+        {ret !== undefined ? (
+          <Link
+            to='/'
+            className='flex items-center transition-transform hover:scale-110'
+          >
+            <HomeIcon className='text-waveAqua2 h-8 w-8' />
+          </Link>
+        ) : (
+          <div />
+        )}
+      </div>
     </header>
   );
 }
