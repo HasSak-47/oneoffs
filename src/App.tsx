96598a1535f32e3d16c6d1fbaeaa5720e3d29236
Ulises Alanis <ulises.alanis.255@gmail.com>
2025-05-26 23:36:03 -0600

    I read bad the docs

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import Notes from './Notes';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Tetris from './Tetris';
 import PalletGenerator from './PalletGenerator';
 import Options from './Options';
 
 export default function App() {
   return (
-    <Router>
+    <BrowserRouter basename='/oneoffs'>
       <Routes>
         <Route path='/' element={<Options />} />
         <Route path='/tetris' element={<Tetris />} />
         <Route path='/PalletGenerator' element={<PalletGenerator />} />
         <Route path='/notes' element={<Notes />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,11 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
-import { BrowserRouter } from 'react-router';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <div className='h-screen w-screen'>
-      <BrowserRouter basename='/oneoffs'>
-        <App />
-      </BrowserRouter>
+      <App />
     </div>
   </StrictMode>
 );
