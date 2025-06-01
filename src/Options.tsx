8d12c6f6b27c8e1e341b05f04f8de12bc34d0f0f
Ulises Alanis <ulises.alanis.255@gmail.com>
2025-06-01 01:49:24 -0600

    added crypto stuff

--- src/Options.tsx
+++ src/Options.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect, useRef, KeyboardEvent } from 'react';
 import { Link } from 'react-router-dom';
 
 function Options() {
-  const links = ['Notes', 'PalletGenerator', 'Blog'];
+  const links = ['Notes', 'PalletGenerator', 'Blog', 'Crypto'];
   const [focusedIndex, setFocusedIndex] = useState<number | null>(null);
   const linkRefs = useRef<Array<HTMLAnchorElement | null>>([]);
   const rootRef = useRef<HTMLDivElement | null>(null);
